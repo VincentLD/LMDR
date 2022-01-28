31ceed3da2d2a7708b99adb7c4dd75eb150c4197
Vincent <vledoledec@gmail.com>
2022-01-28 09:56:54 +0100

    Mise en place du menu (Desktop/Mobile)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector('.burger-bars');
 const nav = document.querySelector('.navbar');
 const links = document.querySelectorAll('.nav__link')
+const links_text = document.querySelectorAll('.links');
 
 burger.addEventListener('click', () => {
   if (nav.classList.contains('show-nav')) {
@@ -8,11 +9,21 @@ burger.addEventListener('click', () => {
   } else {
     nav.classList.add('show-nav');
   }
-})
+});
 
 links.forEach(link => {
   link.addEventListener('click', () => 
     nav.classList.contains('show-nav') 
     ? nav.classList.remove('show-nav')
     : ''
-)})
+  )
+  link.addEventListener('mouseover', () => {
+    const textWidth = link.childNodes[1].offsetWidth;
+    link.style.width =  100 + textWidth + 'px'
+  });
+
+  link.addEventListener('mouseleave', () => {
+    const textWidth = link.childNodes[1].offsetWidth;
+    link.style.width =  70 + 'px'
+  });
+});
